17f993ce187f796be6737f58ca9d0dcfe70793dd
CanardMandarin <thibault.marboud@gmail.com>
2023-01-29 18:29:10 +0000

    Update consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -5,6 +5,9 @@ lazy_static! {
     pub static ref LIMIT_PER_CURRENCY: HashMap<String, f64> = {
         let mut m = HashMap::new();
         m.insert("USD".to_string(), 150.0);
+        m.insert("SOL".to_string(), 1.0);
+        m.insert("BTC".to_string(), 0.00002);
+
         m
     };
 }
