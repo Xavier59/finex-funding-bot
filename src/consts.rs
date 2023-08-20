fe1738cef7eddfc29f427b418fc9c47d3e56126b
Xavier59 <10245003+Xavier59@users.noreply.github.com>
2023-08-20 20:11:27 +0200

    add usdt

--- src/consts.rs
+++ src/consts.rs
@@ -7,7 +7,7 @@ lazy_static! {
         m.insert("USD".to_string(), 150.0);
         m.insert("SOL".to_string(), 1.0);
         m.insert("BTC".to_string(), 0.00002);
-
+        m.insert("USDT".to_string(), 150.0);
         m
     };
 }
